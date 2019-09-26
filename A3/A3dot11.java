2afd804f5e3ced06d94d0553489501e45a5d5208
01duke <idahodukes@hotmail.c>
2019-09-26 13:40:15 -0700

    months

--- A3/A3dot11.java
+++ A3/A3dot11.java
@@ -1,3 +1,4 @@
+// finding the days in a month
 import java.util.Scanner;
 
 public class A3dot11{
@@ -6,13 +7,37 @@ public class A3dot11{
 
     System.out.print("Enter month: ");
          int month = input.nextInt();
-    switch (day + futureDay) {
-         case 0: System.out.println("Sunday"); break;
-         case 1: System.out.println("Monday"); break;
-         case 2: System.out.println("Tuseday"); break;
-         case 3: System.out.println("Wendsday"); break;
-         case 4: System.out.println("Thursday"); break;
-         case 5: System.out.println("Friday"); break;
-         case 6: System.out.println("Saturday");     
-    System.out.print("Enter year: ");
-         int year= input.nextInt();
+   switch (month) {
+         case 1: month January; break;
+         case 2: System.out.println("February"); break;
+         case 3: System.out.println("March"); break;
+         case 4: System.out.println("April"); break;
+         case 5: System.out.println("May"); break;
+         case 6: System.out.println("June"); break;
+         case 7: System.out.println("July"); break;
+         case 8: System.out.println("August"); break;
+         case 9: System.out.println("September"); break;
+         case 10: System.out.println("October"); break;
+         case 11: System.out.println("November"); break;
+         case 12: System.out.println("December");
+   } 
+
+   System.out.print("Enter year: ");
+      int year= input.nextInt();
+   
+      int days = 0;
+   if (month == 1 || month == 3 || month == 5|| month == 7 || month == 8 || month == 10 || month == 12)
+      days = 31;
+      else if(month == 4 || month == 6 || month ==9|| month == 11)
+      days = 30;
+      // Check to see if the year is a leap year
+   boolean leapYear =
+      (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+   if (leapYear = true)
+      days = 29;
+      else
+      days =28;
+      input.close();   
+      System.out.print("In " + month + " " + year + " had " + days + " days." );
+   }
+}
