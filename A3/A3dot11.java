ea10038a811cb6147a4248eedc99399dcc25e1f0
01duke <idahodukes@hotmail.c>
2019-09-26 12:35:16 -0700

    java

--- A3/A3dot11.java
+++ A3/A3dot11.java
@@ -6,5 +6,13 @@ public class A3dot11{
 
     System.out.print("Enter month: ");
          int month = input.nextInt();
+    switch (day + futureDay) {
+         case 0: System.out.println("Sunday"); break;
+         case 1: System.out.println("Monday"); break;
+         case 2: System.out.println("Tuseday"); break;
+         case 3: System.out.println("Wendsday"); break;
+         case 4: System.out.println("Thursday"); break;
+         case 5: System.out.println("Friday"); break;
+         case 6: System.out.println("Saturday");     
     System.out.print("Enter year: ");
          int year= input.nextInt();
