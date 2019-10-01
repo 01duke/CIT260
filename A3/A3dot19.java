76daff7ceae59b3b2341232518195d8f0141de14
O1duke <idahodukes@hotmail.com>
2019-10-01 11:31:23 -0600

    t he to the

--- A3/A3dot19.java
+++ A3/A3dot19.java
@@ -13,7 +13,7 @@ public class A3dot19{
 	 double s3 = input.nextDouble();
 	 if (s1 + s2 > s3 && s2 + s3 > s1 && s3 + s1 > s2){
 		 double perimeter = s1 + s2 + s3;
-		 System.out.println("T he perimeter is: " + perimeter);
+		 System.out.println("The perimeter is: " + perimeter);
 	 }
 	 	else{
 			System.out.println(" That is not a vaild triangle.");
