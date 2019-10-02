71b328a5cfa7562967fd467d21bf154c06526460
O1duke <idahodukes@hotmail.com>
2019-10-02 14:00:16 -0600

    save

--- A3/A3dot5.java
+++ A3/A3dot5.java
@@ -7,16 +7,29 @@ public class A3dot5{
       System.out.print("Enter today's day: ");
          int day = input.nextInt();
       System.out.print("Enter the number of days elapsed since today: ");
-         int futureDay= input.nextInt();
+         int future= input.nextInt();
+         String day_of_week  = "";
+         switch (day_of_week ) {
+            case 0: day_of_week = "Sunday"; break;
+            case 1: day_of_week = "Monday"; break;
+            case 2: day_of_week = "Tuesday"; break;
+            case 3: day_of_week = "Wednesday"; break;
+            case 4: day_of_week = "Thursday"; break;
+            case 5: day_of_week = "Friday"; break;
+            case 6: day_of_week = "Saturday";
+        }
       
-      switch ((day + futureDay) % 7) {
-         case 0: System.out.println("Sunday"); break;
-         case 1: System.out.println("Monday"); break;
-         case 2: System.out.println("Tuseday"); break;
-         case 3: System.out.println("Wendsday"); break;
-         case 4: System.out.println("Thursday"); break;
-         case 5: System.out.println("Friday"); break;
-         case 6: System.out.println("Saturday");
+      
+      int futureDay = (day + future) % 7;
+      String futureDayIs = "";
+         switch ( futureDayIs) {
+            case 0: System.out.println("Sunday"); break;
+            case 1: System.out.println("Monday"); break;
+            case 2: System.out.println("Tuseday"); break;
+            case 3: System.out.println("Wendsday"); break;
+            case 4: System.out.println("Thursday"); break;
+            case 5: System.out.println("Friday"); break;
+            case 6: System.out.println("Saturday");
       }
    input.close();
    }
