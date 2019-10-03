77e89a352acea9edec5579c17f12afe89d1ff308
O1duke <idahodukes@hotmail.com>
2019-10-03 15:37:38 -0600

    ecipse

--- Chapter4/A4dot12.java
+++ Chapter4/A4dot12.java
@@ -1,40 +1,41 @@
 /* 
 taking a Hexadecimal digit and converting
 to Binary.
-*/
+ */
 import java.util.Scanner;
 
 public class A4dot12{
-public static void main(String[] args){
-  Scanner input = new Scanner(System.in);
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
 
-  System.out.print("Enter a Hexadecimal digit: ");
-  int hexDigit = input.nextInt();
-  switch(binary){
-    case 0: binary = 0000; break;
-    case 1: binary = 0001; break;
-    case 2: binary = 0010; break;
-    case 3: binary = 0011; break;
-    case 4: binary = 0100; break;
-    case 5: binary = 0101; break;
-    case 6: binary = 0110; break;
-    case 7: binary = 0111; break;
-    case 8: binary = 1000; break;
-    case 9: binary = 1001; break;
-    case a: 
-    case A: binary = 1010; break;
-    case b:
-    case B: binary = 1011; break;
-    case c: 
-    case C: binary = 1011; break;
-    case d: 
-    case D: binary = 1101; break;
-    case e: 
-    case E: binary = 1110; break;
-    case f:
-    case F: binary = 1111; break;
-  } 
-  input.close();   
-  System.out.print(binary);
-  }
+		System.out.print("Enter a Hexadecimal digit: ");
+		int hexDigit = input.nextInt();
+		String binary = "ERROR";
+		switch(binary ){
+		case 0: binary = "0000"; break;
+		case 1: binary = "0001"; break;
+		case 2: binary = "0010"; break;
+		case 3: binary = "0011"; break;
+		case 4: binary = "0100"; break;
+		case 5: binary = "0101"; break;
+		case 6: binary = "0110"; break;
+		case 7: binary = "0111"; break;
+		case 8: binary = "1000"; break;
+		case 9: binary = "1001"; break;
+		case a: 
+		case A: binary ="1010"; break;
+		case b:
+		case B: binary = "1011"; break;
+		case c: 
+		case C: binary = "1011"; break;
+		case d: 
+		case D: binary = "1101"; break;
+		case e: 
+		case E: binary = "1110"; break;
+		case f:
+		case F: binary = "1111";
+		} 
+		input.close();   
+		System.out.print(binary);
+	}
 }
