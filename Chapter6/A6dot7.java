b90fa7c5f07ca352cb84bc90076d4715684eb20c
01duke <idahodukes@hotmail.com>
2019-10-18 10:22:47 -0700

    A6 done

--- Chapter6/A6dot7.java
+++ Chapter6/A6dot7.java
@@ -9,22 +9,22 @@ public class A6dot7{
         Scanner input = new Scanner(System.in);
 
         System.out.print("The amount invested:");
-         double investmentAmount = input.nextDouble();
+         double investment = input.nextDouble();
          
         System.out.print("Annual interest rate:");
          double yearlyInterestRate = input.nextDouble();
-        System.out.println("Years \tFuture Value" );
+         double monthlyInterestRate =( yearlyInterestRate / 12) / 100 ;
+        System.out.println("Years \t \tFuture Value" );
 
         for (int years = 1; years <= 30; years+=1){
-        System.out.printf( "%1d \t \t %8.2f\n" , years, fv);
+        System.out.printf( "%1d \t \t %8.2f\n" , years , futureInvestmentValue( investment,  monthlyInterestRate,  years ));
         }
         input.close();
     } 
  
         
-    public static futureInvestmentValue(double investmentAmount, double yearlyInterestRate, double monthlyInterestRate, int years ){
-        monthlyInterestRate = yearlyInterestRate * 12;
-        double fv = monthlyInterestRate * Math.pow(monthlyInterestRate, years) ;
+    public static double futureInvestmentValue(double investment, double monthlyInterestRate, int years ){
+        double fv = investment * Math.pow(1 + monthlyInterestRate, years * 12) ;
         return fv;
     }
 
--- Chapter6/A6dot8.java
+++ Chapter6/A6dot8.java
@@ -1,12 +1,28 @@
-public class A6dot7{
+/*
+
+*/
+
+public class A6dot8{
     public static void main(String[] args){
-        System.out.printf("Celsius %10d Fahrenheit %10d | %10d Fahrenheit %10d Celsius");
-        
-    }
-    public static double celToFah(double celsius){
+        System.out.print("Celsius \t Fahrenheit \t | \t Fahrenheit \t Celsius");
+        System.out.println("\n------------------------------------------------------------------");
+        double countFahrenheit = 120;
+
+         for (double countCelsius = 40; countCelsius >= 31; countCelsius -= 1){
+
+        System.out.printf("%3.1f  \t \t %3.1f \t \t | \t %3.1f \t \t %3.2f\n",countCelsius, findFahrenhiet(countCelsius), countFahrenheit, findCelsius(countFahrenheit));
+        countFahrenheit -= 10;}
 
     }
-    public static double fahToCel(double fahrenheit){
-
+    public static double findFahrenhiet(double countCelsius){
+        double fahrenheit = (9.0 / 5) * (countCelsius + 32);
+        return fahrenheit;
     }
+    public static double findCelsius(double countFahrenheit){
+        double celsius = (5.0 / 9) * (countFahrenheit - 32);
+        return celsius;
+    }
+
+
+    
 }
